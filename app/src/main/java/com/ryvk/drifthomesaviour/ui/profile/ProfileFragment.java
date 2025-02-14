1d31d8e34f94fe68b8dc0ace237136a428eab900
Ravidu Vithana <raviduyashith123@gmail.com>
2025-02-14 23:09:35 +0530

    changed the bottom views navigation from default to the required setup

--- app/src/main/java/com/ryvk/drifthomesaviour/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ryvk/drifthomesaviour/ui/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.ryvk.drifthomesaviour.ui.dashboard;
+package com.ryvk.drifthomesaviour.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.ryvk.drifthomesaviour.databinding.FragmentDashboardBinding;
+import com.ryvk.drifthomesaviour.databinding.FragmentProfileBinding;
 
-public class DashboardFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
